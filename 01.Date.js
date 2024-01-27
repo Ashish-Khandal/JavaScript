272b38d4bfbff6462d9a038efacd75c1b9e41515
Ashish-Khandal <ashishsharmavinod@gmail.com>
2024-01-27 12:12:22 +0530

    Commit

--- 01.Date.js
+++ 01.Date.js
@@ -29,9 +29,9 @@ let myTimeStamp =  Date.now();
 // console.log(Math.floor(Date.now()/1000));
 
 let newDate = new Date();
-// console.log(newDate);
-// console.log(newDate.getDate());
-// console.log(newDate.getMonth()+1);
+console.log(newDate);
+console.log(newDate.getDate());
+console.log(newDate.getMonth()+1);
 
 let newFormate = newDate.toLocaleString('Defult',{
     weekday: "long"
